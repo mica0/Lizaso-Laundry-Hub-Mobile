1120b65e9f5a96720faa95ef69bca157caa3f9ee
Admin <Admin@DESKTOP-STAD1GF>
2024-09-12 06:17:41 +0800

    update

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function home() {
   return (
-    <View>
+    <SafeAreaView>
       <Text style={{ fontSize: 40, fontFamily: "poppins-regular" }}>home</Text>
-    </View>
+    </SafeAreaView>
+  //    <View>
+  //    <Text style={{ fontSize: 40, fontFamily: "poppins-regular" }}>home</Text>
+  //  </View>
   );
 }
+
--- app/index.tsx
+++ app/index.tsx
@@ -6,6 +6,7 @@ export default function Index() {
  
 }
 
+
  // return (
   //   // <View
   //   //   style={{
