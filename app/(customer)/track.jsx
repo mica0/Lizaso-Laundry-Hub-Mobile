110cac0c40011ff4015ffb10542269c61c13187d
Guze <velardereynald@gmail.com>
2024-10-13 14:49:08 +0800

    update

--- app/(customer)/track.jsx
+++ app/(customer)/track.jsx
@@ -3,15 +3,30 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 import COLORS from "../../constants/colors";
 import { fonts } from "../../constants/fonts";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Track() {
   return (
-    <SafeAreaView style={{ flex: 1, padding: 16, backgroundColor: "#f5f5f5" }}>
-      <Text
-        style={{ fontSize: 40, fontFamily: fonts.Regular, marginBottom: 16 }}
-      >
-        Explore
-      </Text>
-    </SafeAreaView>
+    <LinearGradient
+      colors={["#5787C8", "#71C7DA"]}
+      locations={[0, 0.8]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1.5, y: 0 }}
+      style={{ flex: 1 }}
+    >
+      <SafeAreaView style={styles.container}>
+        <Text
+          style={{ fontSize: 40, fontFamily: fonts.Regular, marginBottom: 16 }}
+        >
+          Explore
+        </Text>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
